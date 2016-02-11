c8c7152b62006a199a549d3a2a11d5f99a020c3b
Nelson Gomez <ng2573@columbia.edu>
2016-02-10 22:42:47 -0500

    Resolve a circular dependency in machine classes

--- machines.py
+++ machines.py
@@ -1,9 +1,49 @@
 import Logger
-import ReferenceMachine
-
+import models
 import random
 import subprocess
 import time
+
+class ReferenceMachine:
+    def __init__(self, image_name, system_name):
+        # type: (str, str)
+        self._image_name = image_name
+        self._system_name = system_name
+
+
+    def _get_image_name(self):
+        return self._image_name
+
+
+    def _get_system_name(self):
+        return self._system_name
+
+    image_name = property(_get_image_name)
+    system_name = property(_get_system_name)
+
+
+    """
+    Takes the ReferenceMachine and clones it to create a
+    ManagedMachine which can be interacted with by the VM
+    manager to be wiped or removed at random and infected
+    by malware received by email.
+
+    Throws a CalledProcessError exception if VBoxManage
+    cannot clone the reference machine properly.
+    """
+    def clone_as_managed_machine(self, new_image_name):
+        # type: (str) -> ManagedMachine        
+        subprocess.check_output(['VBoxManage', 'clonevm',
+            self._image_name, '--mode', 'machine',
+            '--name', new_image_name, '--register'])
+
+        model = models.ManagedMachine()
+        model.image_name = new_image_name
+        model.reference_image = self._image_name
+        model.save()
+
+        return ManagedMachine(new_image_name, self, model)
+
 
 class ManagedMachine:
     def __init__(self, image_name, reference_image, model):
@@ -47,6 +87,14 @@ class ManagedMachine:
     model = property(_get_model)
 
 
+    def stop_silently(self):
+        self._logger.info('Attempting to stop ' +
+            self._image_name + ' silently')
+
+        subprocess.call(['VBoxManage', 'controlvm',
+            self._image_name, 'poweroff'])
+
+
     def start(self):
         # type: bool
         if self._is_running or self._is_destroyed:
@@ -55,10 +103,13 @@ class ManagedMachine:
         self._logger.info('Starting machine ' + 
             self._image_name)
 
-        subprocess.check_output(['VBoxHeadless',
-            '--startvm', self._image_name])
+        self.stop_silently()
+        subprocess.check_output(['VBoxManage', 'startvm',
+            self._image_name, '--type', 'headless'])
 
         self._is_running = True
+        self._logger.info('Started ' + self._image_name)
+
         return True
 
 
@@ -114,10 +165,10 @@ class ManagedMachine:
         self._logger.info('Resetting machine ' +
             self._image_name + ' to reference state')
 
-        self.destroy_machine()
-
         reference = self._reference_image
         name = self._image_name
+
+        self.destroy_machine()
         return reference.clone_as_managed_machine(name)
 
 
@@ -131,4 +182,5 @@ class ManagedMachine:
         subprocess.check_output(['VBoxManage',
             'unregistervm', self._image_name, '--delete'])
 
+        self._model.delete_instance()
         self._is_destroyed = True
